0cb90c2313d3d0e1669d6eec530358c176bd7720
linda <tkd09152129@gmail.com>
2024-07-23 04:29:39 +0800

    feat: add routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./co/Hero";
+import Hero2 from "./co/Hero2";
 import ThemeController from "./co/ThemeController";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
+    {/* <Hero2 /> */}
     <Hero />
     <ThemeController />
     </main>
